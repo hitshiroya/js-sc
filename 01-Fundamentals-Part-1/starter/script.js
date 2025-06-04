fd200ff52de4a729ca208ecca413bcd1e033dc18
hshiroya <hshiroya@redhat.com>
2025-06-04 16:12:43 +0530

    Done with funda-1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,29 @@
-let msg = "amazing";
+// let msg = "amazing";
 
-if(msg=== 'amazing')alert("you are awesome");
+// if(msg=== 'amazing')alert("you are awesome");
 
 
-console.log(10 + 20 - 30 + 40);
+// console.log(10 + 20 - 30 + 40);
+
+
+// //varaible
+
+// let name  = "Hit";
+// console.log(name);
+
+// let age = 24;
+// console.log(age);
+
+
+// falsy values
+
+// 0,'',null,NaN,undefined
+
+
+let height=100;
+
+if(height){
+    console.log("There is some value atleast");
+}else{
+    console.log("No, it's not defined!")
+}
