bbcf5234ebf33adb3edcecc63b9577076a7abc2b
hit shiroya <hitp363530@gmail.com>
2025-06-11 01:10:29 +0530

    Completed operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 // // // Data needed for a later exercise
 // // const flights =
@@ -30,8 +30,8 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order : function(starterIndex,mainIndex) {
-    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
   openingHours: {
@@ -50,9 +50,9 @@ const restaurant = {
   },
 };
 
-const {openingHours} = restaurant;
-const {thu,fri} = openingHours;
-console.log(thu,fri)
+const { openingHours } = restaurant;
+const { thu, fri } = openingHours;
+console.log(thu, fri);
 
 // // const {mainMenu,categories} = restaurant;
 // // console.log(mainMenu,categories);
@@ -65,13 +65,9 @@ console.log(thu,fri)
 // // // [firstPref,secondPref] = [secondPref,firstPref];
 // // // console.log(firstPref,secondPref);
 
-
 // // // const num = [1,2,[3,4]];
 // // // const [i, ,[j,k]] = num;
 // // // console.log(i,j,k);
-
-
-
 
 // // const [firstBook,secondBook] = books;
 // // console.log(firstBook,secondBook);
@@ -87,7 +83,6 @@ console.log(thu,fri)
 // // const [fiveStarRatings,oneStarRatings,threeStarRatings=0] = ratingStars;
 // // console.log(fiveStarRatings,oneStarRatings,threeStarRatings);
 
-
 const books = [
   {
     title: 'Algorithms',
@@ -95,7 +90,16 @@ const books = [
     publisher: 'Addison-Wesley Professional',
     publicationDate: '2011-03-24',
     edition: 4,
-    keywords: ['computer science', 'programming', 'algorithms', 'data structures', 'java', 'math', 'software', 'engineering'],
+    keywords: [
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
+    ],
     pages: 976,
     format: 'hardcover',
     ISBN: '9780321573513',
@@ -108,18 +112,28 @@ const books = [
         ratingsCount: 1733,
         reviewsCount: 63,
         fiveStarRatingCount: 976,
-        oneStarRatingCount: 13
-      }
+        oneStarRatingCount: 13,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
     title: 'Structure and Interpretation of Computer Programs',
-    author: ['Harold Abelson', 'Gerald Jay Sussman', 'Julie Sussman (Contributor)'],
+    author: [
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
+    ],
     publisher: 'The MIT Press',
     publicationDate: '2022-04-12',
     edition: 2,
-    keywords: ['computer science', 'programming', 'javascript', 'software', 'engineering'],
+    keywords: [
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
+    ],
     pages: 640,
     format: 'paperback',
     ISBN: '9780262543231',
@@ -132,18 +146,25 @@ const books = [
         ratingsCount: 14,
         reviewsCount: 3,
         fiveStarRatingCount: 8,
-        oneStarRatingCount: 0
-      }
+        oneStarRatingCount: 0,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
-    title: 'Computer Systems: A Programmer\'s Perspective',
-    author: ['Randal E. Bryant', 'David Richard O\'Hallaron'],
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
     publisher: 'Prentice Hall',
     publicationDate: '2002-01-01',
     edition: 1,
-    keywords: ['computer science', 'computer systems', 'programming', 'software', 'C', 'engineering'],
+    keywords: [
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
+    ],
     pages: 978,
     format: 'hardcover',
     ISBN: '9780130340740',
@@ -156,10 +177,10 @@ const books = [
         ratingsCount: 1010,
         reviewsCount: 57,
         fiveStarRatingCount: 638,
-        oneStarRatingCount: 16
-      }
+        oneStarRatingCount: 16,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
     title: 'Operating System Concepts',
@@ -167,7 +188,15 @@ const books = [
     publisher: 'John Wiley & Sons',
     publicationDate: '2004-12-14',
     edition: 10,
-    keywords: ['computer science', 'operating systems', 'programming', 'software', 'C', 'Java', 'engineering'],
+    keywords: [
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
+    ],
     pages: 921,
     format: 'hardcover',
     ISBN: '9780471694663',
@@ -180,9 +209,9 @@ const books = [
         ratingsCount: 2131,
         reviewsCount: 114,
         fiveStarRatingCount: 728,
-        oneStarRatingCount: 65
-      }
-    }
+        oneStarRatingCount: 65,
+      },
+    },
   },
   {
     title: 'Engineering Mathematics',
@@ -203,10 +232,10 @@ const books = [
         ratingsCount: 370,
         reviewsCount: 18,
         fiveStarRatingCount: 211,
-        oneStarRatingCount: 6
-      }
+        oneStarRatingCount: 6,
+      },
     },
-    highlighted: true
+    highlighted: true,
   },
   {
     title: 'The Personal MBA: Master the Art of Business',
@@ -224,16 +253,23 @@ const books = [
         ratingsCount: 40119,
         reviewsCount: 1351,
         fiveStarRatingCount: 18033,
-        oneStarRatingCount: 1090
-      }
-    }
+        oneStarRatingCount: 1090,
+      },
+    },
   },
   {
     title: 'Crafting Interpreters',
     author: 'Robert Nystrom',
     publisher: 'Genever Benning',
     publicationDate: '2021-07-28',
-    keywords: ['computer science', 'compilers', 'engineering', 'interpreters', 'software', 'engineering'],
+    keywords: [
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
+    ],
     pages: 865,
     format: 'paperback',
     ISBN: '9780990582939',
@@ -244,9 +280,9 @@ const books = [
         ratingsCount: 253,
         reviewsCount: 23,
         fiveStarRatingCount: 193,
-        oneStarRatingCount: 0
-      }
-    }
+        oneStarRatingCount: 0,
+      },
+    },
   },
   {
     title: 'Deep Work: Rules for Focused Success in a Distracted World',
@@ -265,30 +301,157 @@ const books = [
         ratingsCount: 144584,
         reviewsCount: 11598,
         fiveStarRatingCount: 63405,
-        oneStarRatingCount: 1808
-      }
+        oneStarRatingCount: 1808,
+      },
     },
-    highlighted: true
-  }
+    highlighted: true,
+  },
 ];
 
 const firstBook = books[0];
 console.log(firstBook);
 
-const {title,author,ISBN} = firstBook;
-console.log(title,author,ISBN);
+const { title, author, ISBN } = firstBook;
+console.log(title, author, ISBN);
 
-const {keywords:tag} = firstBook;
+const { keywords: tag } = firstBook;
 console.log(tag);
 
 const sevenBook = books[6];
-const {language,programmingLanguage='unknown'} = sevenBook;
-console.log(language,programmingLanguage);
-
-
+const { language, programmingLanguage = 'unknown' } = sevenBook;
+console.log(language, programmingLanguage);
 
 let bookTitle = 'unknown';
 let bookAuthor = 'unknown';
 
-({title:bookTitle,author:bookAuthor} = firstBook);
-console.log(title,author);
+({ title: bookTitle, author: bookAuthor } = firstBook);
+console.log(title, author);
+
+const arr = [1, 2, 3, 4, 5];
+
+const newArr = [0, ...arr];
+console.log(newArr);
+
+const mixMenu = ['Khaman', 'Dhokla', ...restaurant.mainMenu];
+console.log(mixMenu);
+
+const copyMainMenu = [...restaurant.mainMenu];
+
+const finalMenu = [...mixMenu, ...copyMainMenu];
+console.log(finalMenu);
+
+function add(num1, num2, ...arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total + num1 + num2;
+}
+const ans = add(2, 3, 4, 5, 6, 7, 8, 9, 0);
+console.log(ans);
+
+let maxScore = undefined;
+const targetScore = maxScore || 25;
+console.log(targetScore);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+
+
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const player1 = [...game.players[0]];
+const player2 = [...game.players[1]];
+
+const [gk,...fieldPlayer] = [...game.players[0]];
+console.log(gk,fieldPlayer);
+
+const allPlayers = [...player1,...player2];
+console.log(allPlayers);
+
+const players1Final = [...player1,'Thiago', 'Coutinho', 'Perisic']
+console.log(players1Final);
+
+const {team1,x,team2} = game.odds;
+console.log(team1,x,team2);
+
+console.log(player1);
+
+
+
+function printGoals(...p){
+  console.log(p);
+  let goals = 0;
+  for(let i=0;i<p.length;i++){
+    goals += p[i];
+    
+  }
+  return goals;
+}
+
+//const g = [1,2,3,4,5,6,7,8,9,10];
+const totalGoals = printGoals(1,2,3,4,5,6,7,8,9,10);
+console.log(totalGoals);
