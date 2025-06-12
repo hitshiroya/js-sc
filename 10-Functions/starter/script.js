8c417e638efde1d47c7e5e0fe87e386a82e9f7e9
hit shiroya <hitp363530@gmail.com>
2025-06-13 01:33:43 +0530

    Started internal of function is js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+let bookingList = [];
+const flightBooking = function(source,destination,roundTrip = true){
+    const bookingObject = {
+        source,
+        destination,
+        roundTrip
+    }
+
+    bookingList.push(bookingObject);
+    return bookingList;
+}
+
+
+const trip1 = flightBooking("Ahmedabad","London");
+console.log(trip1);
+
+
+// higher order function 
+
+
+// const checkPsg = function (psg){
+//     if(psg == 'Hit'){
+//         console.log("Data verified...");
+//         console.log("Psg is valid...please allow entry");
+//         return true;
+//     }
+// }
+// const digiYatraCheck = function(psg,fn){
+//     let user = fn(psg);
+//     console.log(`${user=psg}'s checkin done...`);
+//     console.log(fn.name);
+//     return true;
+// }
+
+// const psgFlight = digiYatraCheck("Hit",checkPsg);
+// console.log(psgFlight);
+
+
+// function greet (name){
+//     return function moshi(lang){
+//         console.log(`${lang} ${name}`);
+//         return true;
+//     }
+// }
+
+// const user = greet("Hit")("moshi moshi");
+// console.log(user);
+
+
+const greet = (name) => {
+    return (lang) => {
+        console.log(`${lang} ${name}`);
+    }
+}
+
+const user = greet("Hit")("moshi moshi");
