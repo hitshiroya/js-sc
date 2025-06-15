d7ae5d6fd5dbd800a94eabd08626224f34dae1c6
hit shiroya <hitp363530@gmail.com>
2025-06-15 23:50:19 +0530

    Completed project till value rendering

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,184 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//movements of transaction
+const updateMovements = (movementsArary) => {
+  containerMovements.innerHTML = '';
+  movementsArary.forEach((val, idx) => {
+    console.log(movementsArary,"array inside function");
+    let type = val >= 0 ? 'deposit' : 'withdrawal';
+
+    let movementEle = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">
+          ${idx + 1} ${type} </div>
+          <div class="movements__value">${val}€</div>
+    </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', movementEle);
+  });
+};
+
+//calculate total balance and prit it
+const calculateBalance = arr => {
+  return arr.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+};
+
+//calculate income summary
+
+const calculateSummary = (arr,it) => {
+  
+  const incoming = arr
+    .filter(income => {
+      return income > 0;
+    })
+    .reduce((acc, val) => {
+      return acc + val;
+    }, 0);
+
+  
+
+  labelSumIn.textContent = incoming;
+
+  const outgoing = arr
+    .filter(income => {
+      return income < 0;
+    })
+    .reduce((acc, val) => {
+      return acc + val;
+    }, 0);
+
+  labelSumOut.textContent = Math.abs(outgoing);
+
+  const interest = arr
+    .filter(val => {
+      return val > 0;
+    })
+    .map(i => {
+      return (i * it ) / 100;
+    })
+    .filter(iin => {
+      return iin >= 1;
+    })
+    .reduce((acc, val) => {
+      return acc + val;
+    }, 0);
+
+  labelSumInterest.textContent = `${interest} %`;
+};
+
+let currentLoggedInUser;
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+
+  accounts.find(account => {
+    if (
+      account.username === inputLoginUsername.value &&
+      account.pin === Number(inputLoginPin?.value)
+    ) {
+      currentLoggedInUser = account.username;
+      inputLoginUsername.value = '';
+      inputLoginPin.value = '';
+      labelWelcome.textContent = `Welcome,${account.owner.split(' ')[0]}`;
+      containerApp.style.opacity = 100;
+
+      updateMovements(account.movements);
+
+      const totalBalance = calculateBalance(account.movements);
+      labelBalance.textContent = `${totalBalance}€`;
+
+      calculateSummary(account.movements,account.interestRate);
+    }
+  });
+
+  btnTransfer.addEventListener("click",(e)=>{
+    e.preventDefault();
+
+    accounts.find((account)=>{
+      if(account.username === inputTransferTo.value){
+        const amount = Number(inputTransferAmount.value);
+        accounts.find((currentUser)=>{
+          if(currentUser.username === currentLoggedInUser){
+            currentUser.movements.push(amount * -1);
+            updateMovements(currentUser.movements);
+            const totalBalance = calculateBalance(currentUser.movements);
+            labelBalance.textContent = `${totalBalance}€`;
+    
+            calculateSummary(currentUser.movements,account.interestRate);
+            console.log(currentUser.movements);
+          }
+        })
+        account.movements.push(amount);
+        updateMovements(account.movements);
+        const totalBalance = calculateBalance(account.movements);
+        labelBalance.textContent = `${totalBalance}€`;
+
+        calculateSummary(account.movements,account.interestRate);
+
+
+      }
+    })
+    
+  })
+});
+
+//compute usernames
+const computeUserName = accounts => {
+  accounts.forEach(usr => {
+    usr.username = usr.owner
+      .toLowerCase()
+      .split(' ')
+      .map(u => {
+        return u[0];
+      })
+      .join('');
+  });
+};
+
+const userNames = computeUserName(accounts);
+console.log(accounts);
+
+//design logging flow
+
+const userName = inputLoginUsername.value;
+const userPin = inputLoginPin.value;
+
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+  if (
+    (userName == account1.userName && userPin == account1.pin) ||
+    (userName == account2.userName && userPin == account2.pin) ||
+    (userName == account3.userName && userPin == account3.pin)
+  ) {
+    containerApp.style.opacity = 100;
+  }
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// currencies.forEach((val, key) => {
+//   console.log(`Key is ${key} and value is ${val}`);
+// });
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const deposits = movements
+//   .filter(amount => {
+//     return amount > 0;
+//   })
+//   .reduce((acc, val) => {
+//     return acc + val;
+//   }, 0);
+// console.log(deposits);
 
 /////////////////////////////////////////////////
