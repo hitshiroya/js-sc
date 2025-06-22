668339021b511e5f0554ad65cc440f0ffd67f3fb
hit shiroya <hitp363530@gmail.com>
2025-06-22 22:32:30 +0530

    Completed prototype

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -5,17 +5,57 @@ const Person = function(fulllName,dateOfBirth){
     this.fulllName = fulllName;
     this.dateOfBirth = dateOfBirth;
 
-    this.calcAge = function(){
-        console.log(2024-this.dateOfBirth);
-    }
 }
 
 const hit  = new Person("Hit Shiroya","March-2002");
 
-console.log(hit);
+
+
 // What will happen behind the scene
 
 // 1. new object will be created {}
 // 2. function will call, this willl be form which will point to {} 
 // 3. prototype linking
 // 4. Return object with value
+
+
+Person.prototype.calcAge = function() {
+    console.log("Prototype Method....");
+}
+
+console.log(hit.__proto__)
+
+
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+
+const Car = function(make,speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function() {
+    console.log(`Speed has been increased to ${this.speed + 10}`);
+}
+
+Car.prototype.brake = function() {
+    console.log(`Speed has been increased to ${this.speed - 5}`);
+}
+
+const bmw = new Car("BMW",100);
+bmw.accelerate();
+
+const audi = new Car("Audi",80);
+audi.brake();
