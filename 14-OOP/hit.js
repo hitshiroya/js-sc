4d8fe9ed7ba20ea549ed81597689f1efe114c167
hit shiroya <hitp363530@gmail.com>
2025-07-18 15:00:45 +0530

    h

---
+++ 14-OOP/hit.js
@@ -0,0 +1,7 @@
+var name = "test"  // Missing semicolon + var usage
+console.log("Debug message"); // Console.log detection
+// TODO: Fix this later      // TODO detection
+
+function hello() {
+    return "world"  // Missing semicolon
+}
